dd2c2eac1adfc86762e9b2173e4274aa8b871687
Maria Rita <mariaritacfwork@gmail.com>
2025-11-24 19:28:33 -0300

    Subindo nova pasta e exercicios

---
+++ MyfirstProject/src/ONE.java
@@ -0,0 +1,17 @@
+public class ONE {
+    public static void main(String[] args) throws Exception {
+        
+        // This is my first JAVA PROGRAM
+
+        /*
+            THIS
+                IS 
+                    MULTI-LINE
+                            COMMENT  :)     
+        */
+
+        System.out.print("Hello, World!\n");
+        System.out.println("I like very pizza");
+        System.out.print("It's really good");
+    }
+}
